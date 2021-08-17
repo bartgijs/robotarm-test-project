848da00f6a048d9925a481ef1549e4047889b96c
Bart Gijsbertsen <bartgijsbertsen@gmail.com>
2021-08-17 13:37:20 +0200

    Integration test with robotarm library structure (success)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
+#include "integration.hpp"
 
 void setup() {
-  // put your setup code here, to run once:
+	Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	printTestMessage();
 }
